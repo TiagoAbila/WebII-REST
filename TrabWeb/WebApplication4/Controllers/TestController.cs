3015255e9aec3555146367027b55ca0214d19632
Tiago Leandro Abila <tlabila@hotmail.com>
2020-10-30 20:44:23 -0300

    Swagger funcionando

---
+++ TrabWeb/WebApplication4/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet("nome")]
+        public IActionResult Get()
+        {  
+            return Ok(new { name = "Tiago"});
+        }
+    }
+}
